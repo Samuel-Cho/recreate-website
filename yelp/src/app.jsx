52f43aa174d1a17a1563789e0ba776214f608cbf
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-06-02 22:49:42 +0000

    Edit project structure for Yelp.

---
+++ yelp/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Homepage from './pages/homepage';
+
+export default class App extends React.Component{
+  render() {
+    return (
+      <Homepage />
+    );
+  }
+}
