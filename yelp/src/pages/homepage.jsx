86e58174f4ac98e76eb9431afb979d82cb71ee8d
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-06-02 23:51:11 +0000

    Started creating elements for homepage search area.

--- yelp/src/pages/homepage.jsx
+++ yelp/src/pages/homepage.jsx
@@ -3,6 +3,29 @@ import ReactDom from 'react-dom';
 
 export default class Homepage extends React.Component{
   render(){
-    return <button>Test</button>
+    return (
+      <div className='homepage'>
+        <div className='row header-top'>
+          <div className='content'>
+            <div className='top-nav'>
+              <div className='top-nav-link-container'>
+                <a className='top-nav-link' href=''>Write a Review</a>
+                <a className='top-nav-link' href=''>Events</a>
+                <a className='top-nav-link' href=''>Talk</a>
+              </div>
+              <div className='top-nav-log-container'>
+                <a className='log-in' href=''>Log In</a>
+                <a className='sign-up' href=''>Sign Up</a>
+              </div>
+            </div>
+            <div className='search-row'></div>
+            <div className='photo-source'>
+              <p className='photo-location'>Melbourne Food Porn Festival</p>
+              <p className='photographer'><span className='photo-by'>Photo by</span> Sharon H.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 };
