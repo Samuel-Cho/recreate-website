b1985fb20f02faba12162954fa2fbc97048dbf82
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-06-03 22:55:52 +0000

    Added yelp logo, input fields, and started suggestion list. #1

--- yelp/src/pages/homepage.jsx
+++ yelp/src/pages/homepage.jsx
@@ -18,7 +18,32 @@ export default class Homepage extends React.Component{
                 <a className='sign-up' href=''>Sign Up</a>
               </div>
             </div>
-            <div className='search-row'></div>
+            <div className='search-row'>
+              <div className='logo-container'>
+                <img className='yelp-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/1280px-Yelp_Logo.svg.png'></img>
+              </div>
+              <div className='search-container'>
+                <div className='input-container'>
+                  <form className='search-form'>
+                    <input type='text' name='find' id='find' placeholder='plumbers, delivery, takeout...'></input>
+                    <input type='text' name='near' id='near' placeholder='La Habra Heights, CA 90631'></input>
+                    <button type='button' className='search-button'>
+                      <span><i class="fas fa-search"></i></span>
+                    </button>
+                  </form>
+                </div>
+                <div className='suggestion-list-container'>
+                  <ul className='suggestion-list'>
+                    <li className='suggestion'>
+                      <a className='suggestion-link' href=''>
+                        <i class="fas fa-faucet suggestion-icon"></i>
+                        <p className='suggestion-text'>Plumbers</p>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
             <div className='photo-source'>
               <p className='photo-location'>Melbourne Food Porn Festival</p>
               <p className='photographer'><span className='photo-by'>Photo by</span> Sharon H.</p>
