2efb37c82db9987d143f08c95c474bc1c0329875
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-06-09 19:03:44 +0000

    Started styling top row of menu container and fixed layout error.

--- yelp/src/pages/businessPage.jsx
+++ yelp/src/pages/businessPage.jsx
@@ -10,12 +10,20 @@ export default class BusinessPage extends React.Component {
             <img alt='yelp logo' className='yelp-logo-menu' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/1280px-Yelp_Logo.svg.png'></img>
             <div className='input-container'>
               <form className='search-form'>
-                <input type='text' name='find' id='find' placeholder='plumbers, delivery, takeout...'></input>
-                <input type='text' name='near' id='near' placeholder='La Habra Heights, CA 90631'></input>
-                <button type='button' className='search-button'>
-                  <span><i class="fas fa-search"></i></span>
+                <input type='text' name='type' id='type' placeholder="tacos, cheap dinner, Max's"></input>
+                <input type='text' name='location' id='location' placeholder='Irvine, CA'></input>
+                <button type='button' className='bd-search-button'>
+                  <span><i class="fas fa-search bd-search"></i></span>
                 </button>
               </form>
+            </div>
+            <div className='business-review-link-container'>
+              <a className='for-businesses' href='#businessPage'>For Businesses</a>
+              <a className='write-review' href='#businessPage'>Write a Review</a>
+            </div>
+            <div className='button-container'>
+              <a className='bp-log-in' href='#businessPage'>Log In</a>
+              <a className='bp-sign-up' href='#businessPage'>Sign Up</a>
             </div>
           </div>
           <div className='bottom-row'>
