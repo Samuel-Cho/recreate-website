50e999f1a8f88b5268f66dc5193bdb56aad43372
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-06-09 19:23:12 +0000

    Styled top row of menu container to website.

--- yelp/src/pages/businessPage.jsx
+++ yelp/src/pages/businessPage.jsx
@@ -7,13 +7,15 @@ export default class BusinessPage extends React.Component {
       <div className='business-page'>
         <div className='menu-container'>
           <div className='top-row'>
-            <img alt='yelp logo' className='yelp-logo-menu' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/1280px-Yelp_Logo.svg.png'></img>
+            <a href=' '>
+              <img alt='yelp logo' className='yelp-logo-menu' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/1280px-Yelp_Logo.svg.png'></img>
+            </a>
             <div className='input-container'>
-              <form className='search-form'>
+              <form className='search-form bp-search-form'>
                 <input type='text' name='type' id='type' placeholder="tacos, cheap dinner, Max's"></input>
                 <input type='text' name='location' id='location' placeholder='Irvine, CA'></input>
-                <button type='button' className='bd-search-button'>
-                  <span><i class="fas fa-search bd-search"></i></span>
+                <button type='button' className='bp-search-button'>
+                  <span><i class="fas fa-search bp-search"></i></span>
                 </button>
               </form>
             </div>
@@ -27,7 +29,7 @@ export default class BusinessPage extends React.Component {
             </div>
           </div>
           <div className='bottom-row'>
-
+            <p>test</p>
           </div>
         </div>
         <div className='business-page-content'>
